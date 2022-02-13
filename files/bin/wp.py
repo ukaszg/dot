b9091c0140871afcdf725b884d1c7093a805f6f2
Łukasz Gruner <lukasz@gruner.lu>
2022-02-13 23:04:23 +0100

    refactor

--- files/bin/wp.py
+++ files/bin/wp.py
@@ -6,5 +6,5 @@ from random import choice
 if __name__ == '__main__':
     wp_dir = "/".join([environ['HOME'], "pic/wp"])
     if path.isdir(wp_dir):
-        wallpaper = "/".join([wp_dir, choice(listdir(wp_dir))])
-        system(" ".join(["hsetroot -full", wallpaper.replace(' ', r'\ ')]))
+        wallpaper = "/".join([wp_dir, choice(listdir(wp_dir))]).replace(' ', r'\ ')
+        system(" ".join(["hsetroot -full", wallpaper]))
