e998b03a214625ac880dbe89ac4761646bca47f9
Łukasz Gruner <lukasz@gruner.lu>
2022-05-19 18:38:13 +0200

    format winenv.sh

--- files/bin/winenv.sh
+++ files/bin/winenv.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 TRANSLATE=0
 [ $1 = '-p' ] && {
@@ -7,14 +7,16 @@ TRANSLATE=0
 }
 
 [ $# = 1 ] || {
-    echo "Usage:    $0 [options] 'ENV_NAME'" >&2
-    echo "      Options:" >&2
-    echo "      -p  translate from windows path to wsl" >&2
+    echo "Usage: $0 [options] ENV_NAME" >&2
+    echo "  Options:" >&2
+    echo "  -p  translate from windows path to wsl" >&2
     exit 1
 }
 
 VALUE=$(powershell.exe "[System.Environment]::GetEnvironmentVariable(\"$1\")")
 
-[ "$TRANSLATE" = "1" ] && { VALUE=$(wslpath -a -u "$VALUE"); }
+[ "$TRANSLATE" = "1" ] && {
+    VALUE=$(wslpath -a -u "$VALUE")
+}
 
 exec echo "$VALUE" | tr -d '\r'
