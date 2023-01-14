a34c28a03afb2e0aa241e492303d95ac2e3626fa
Łukasz Gruner <lukasz@gruner.lu>
2023-01-15 00:19:05 +0100

    emacs

--- files/bin/runemacs.sh
+++ files/bin/runemacs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-FILENAME=$(wslpath -u "$1")
+if [ -z $1 ]; then
+    setsid emacs
+else
+    FILENAME=$(wslpath -u "$1")
 
-pidof emacs 1>/dev/null 2>&1 && \
-    emacsclient -n $FILENAME || \
-    setsid emacs $FILENAME
+    pidof emacs 1>/dev/null 2>&1 && \
+        emacsclient -n $FILENAME || \
+        setsid emacs $FILENAME
+fi
