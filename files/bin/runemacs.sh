5c7178fdba9c406e8725f0072a78ffd5f2df6943
Łukasz Gruner <lukasz@gruner.lu>
2023-01-25 16:43:53 +0100

    snap

--- files/bin/runemacs.sh
+++ files/bin/runemacs.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 if [ -z $1 ]; then
-    setsid emacs
+    exec emacs
 else
     FILENAME=$(wslpath -u "$1")
 
     pidof emacs 1>/dev/null 2>&1 && \
         emacsclient -n $FILENAME || \
-        setsid emacs $FILENAME
+        exec emacs $FILENAME
 fi
